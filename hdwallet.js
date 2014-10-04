bde7b63622b9f0bc9955bb8a035db71b8b7fdf8b
Tim Lee <tim@blockchain.info>
2014-10-03 20:50:27 -0700

    buildHDWallet and setter/getters

--- hdwallet.js
+++ hdwallet.js
@@ -2,17 +2,54 @@ function HDAccount(wallet, label) {
     var accountObject = {
         wallet : wallet,
         label : label,
+        archived : false,
         getLabel : function() {
             return this.label;
         },
         setLabel : function(label) {
             this.label = label;
         },
+        isArchived : function() {
+            return this.archived;
+        },
+        setIsArchived : function(archived) {
+            this.archived = archived;
+        },
+        getAddresses : function() {
+            return this.wallet.addresses;
+        },
+        getAddressesCount : function() {
+            return this.wallet.addresses.length;
+        },
+        getChangeAddresses : function() {
+            return this.wallet.changeAddresses;
+        },
+        getChangeAddressesCount : function() {
+            return this.wallet.changeAddresses.length;
+        },        
         getAccountMainKey : function() {
             return this.wallet.getExternalAccount().toBase58();
         },
         getAccountChangeKey : function() {
             return this.wallet.getInternalAccount().toBase58();
+        },
+        generateAddress : function() {
+            return this.wallet.generateAddress();
+        },
+        undoGenerateAddress : function() {
+            return this.wallet.addresses.pop();
+        },
+        generateChangeAddress : function() {
+            return this.wallet.generateChangeAddress();
+        },
+        getUnspentOutputs : function() {
+            return this.wallet.getUnspentOutputs();
+        },        
+        setUnspentOutputs : function(utxo) {
+            return this.wallet.setUnspentOutputs(utxo);
+        },                
+        getBalance : function() {
+            return this.wallet.getBalance();
         }
     };
 
@@ -23,6 +60,9 @@ function HDWallet(seed) {
     var walletObject = {
         accountArray : [],
         seed : seed,
+        getAccountsCount : function() {
+            return this.accountArray.length;
+        },
         getAccount : function(accountIdx) {
             return this.accountArray[accountIdx];
         },
@@ -37,20 +77,76 @@ function HDWallet(seed) {
             var account = HDAccount(walletAccount, label);
             this.accountArray.push(account);
 
-            return walletAccount;
+            return account;
         }
     };
 
     return walletObject;
 }
 
+function buildHDWallet(passphrase, accountsArrayPayload) {
+    var seed = Bitcoin.crypto.sha256(passphrase);
+    var hdwallet = HDWallet(seed);   
+
+    for (var i = 0; i < accountsArrayPayload.length; i++) {
+        if (archived == true)
+            continue;
+
+        var accountPayload = accountsArrayPayload[i];
+        var label = accountPayload.label;
+        var archived = accountPayload.archived;
+        var external_addresses = accountPayload.external_addresses;
+        var change_addresses = accountPayload.change_addresses;
+        
+        console.log("label: ", label);
+
+        var hdaccount = hdwallet.createAccount(label);
+        hdaccount.setIsArchived(archived);
+
+        for (var j = 0; j < external_addresses; j++) {
+            var address = hdaccount.generateAddress();
+            console.log("\taddress: ", address);
+        }
+
+        for (var k = 0; k < change_addresses; k++) {
+            var changeAddress = hdaccount.generateChangeAddress();
+            console.log("\tchangeAddress: ", address);
+        }
+    }
+
+    return hdwallet;
+}
+
 function test() {
     var passphrase = "don't use a string seed like this in real life";
-    var seed = Bitcoin.crypto.sha256(passphrase);
-    var hdwallet = HDWallet(seed);
+    console.log("passphrase: ", passphrase);
+    var accountsArrayPayload = [
+        {
+            label: "Savings", 
+            archived: false,
+            external_addresses: 7,
+            change_addresses: 12
+        },
+        {
+            label: "archived", 
+            archived: true,
+            external_addresses: 3,
+            change_addresses: 3
+        },
+        {
+            label: "Splurge", 
+            archived: false,
+            external_addresses: 5,
+            change_addresses: 2
+        }
+        
+    ];
+
+    var hdwallet = buildHDWallet(passphrase, accountsArrayPayload);
     hdwallet.createAccount("Rothbard");
     console.log("getAccountMainKey: ", hdwallet.getAccount(0).getAccountMainKey());
     console.log("getAccountChangeKey: ", hdwallet.getAccount(0).getAccountChangeKey());
+
 }
 
 test();
