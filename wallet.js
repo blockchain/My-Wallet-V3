8bb40d886297cdbd64bebe33d6f0d65e55279d36
Tim Lee <tim@blockchain.info>
2014-11-14 13:08:17 -0800

    reenable Subscribe to transactions to legacy addresses

--- wallet.js
+++ wallet.js
@@ -1002,7 +1002,7 @@ var MyWallet = new function() {
             try {
                 var addrs = MyWallet.getActiveAddresses();
                 for (var key in addrs) {
-                    //msg += '{"op":"addr_sub", "addr":"'+ addrs[key] +'"}'; //Subscribe to transactions updates through websockets
+                    msg += '{"op":"addr_sub", "addr":"'+ addrs[key] +'"}'; //Subscribe to transactions updates through websockets
                 }
                 MyWallet.listenToHDWalletAccounts();
             } catch (e) {
