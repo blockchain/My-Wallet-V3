e5871acb6ca9c8a761153ffdd5bef7e1e258d133
Tim Lee <tim@blockchain.info>
2014-11-18 09:38:10 -0800

    Consume change value when less than Standard Output Size

--- signer.js
+++ signer.js
@@ -1152,6 +1152,7 @@ function initNewTx() {
         is_cancelled : false,
         base_fee : Bitcoin.BigInteger.valueOf(10000),
         min_free_output_size : Bitcoin.BigInteger.valueOf(1000000),
+        min_non_standard_output_size : Bitcoin.BigInteger.valueOf(5460),
         allow_adjust : true,
         ready_to_send_header : 'Transaction Ready to Send.',
         min_input_confirmations : 0,
@@ -1410,7 +1411,10 @@ function initNewTx() {
 
                 //Now deal with the change
                 var	changeValue = availableValue.subtract(txValue);
-                if (changeValue.compareTo(Bitcoin.BigInteger.ZERO) > 0) {
+
+                //Consume the change if it would create a very small none standard output
+                //Perhaps this behaviour should be user specified                
+                if (changeValue.compareTo(self.min_non_standard_output_size) > 0) {
                     if (self.change_address != null) //If change address speicified return to that
                         sendTx.addOutput(self.change_address, parseInt(changeValue));
                     else if (addresses_used.length > 0) { //Else return to a random from address if specified
