45e05cf4d633d5d7695114a6d626ace52fa01a38
Tim Lee <tim@blockchain.info>
2014-12-01 13:30:06 -0800

    hdwalletaccount wip

---
+++ HDWalletAccount.js
@@ -0,0 +1,45 @@
+function HDWalletAccount(seed, network) {
+  var assert = Bitcoin.assert;
+  var Address = Bitcoin.Address;
+  var HDNode = Bitcoin.HDNode;
+  var Transaction = Bitcoin.Transaction;
+  var Transaction = Bitcoin.Transaction;
+  var networks = Bitcoin.networks;
+
+  network = network || networks.bitcoin
+
+  // Stored in a closure to make accidental serialization less likely
+  var masterkey = null
+  var me = this
+  this.accountZero = null
+  this.internalAccount = null
+  this.externalAccount = null
+
+  // Addresses
+  this.addresses = []
+  this.changeAddresses = []
+
+  // Transaction output data
+  this.outputs = {}
+
+  // Make a new master key
+  this.newMasterKey = function(seed) {
+    seed = seed || crypto.randomBytes(32)
+    masterkey = Bitcoin.HDNode.fromSeedBuffer(seed, network)
+
+    // HD first-level child derivation method should be hardened
+    // See https://bitcointalk.org/index.php?topic=405179.msg4415254#msg4415254
+    this.accountZero = masterkey.deriveHardened(0)
+    this.externalAccount = this.accountZero.derive(0)
+    this.internalAccount = this.accountZero.derive(1)
+
+    me.addresses = []
+    me.changeAddresses = []
+
+    me.outputs = {}
+  }
+
+  this.newMasterKey(seed)
+
+
+}
