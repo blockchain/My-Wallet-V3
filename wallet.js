ddab3ee91d426099d15e1c5d40100b16e998bc70
Tim Lee <tim@blockchain.info>
2014-12-29 18:57:42 -0800

    off by 1 for settting receiveAddressCount and changeAddressCount

--- wallet.js
+++ wallet.js
@@ -1062,10 +1062,10 @@ var MyWallet = new function() {
                     var path = output.xpub.path.split("/");
                     path[1] = parseInt(path[1]);
                     path[2] = parseInt(path[2]);
-                    if (path[1] == 0 && account.receiveAddressCount < path[2]) {
-                        account.receiveAddressCount = path[2];
-                    } else if (path[1] == 1 && account.changeAddressCount < path[2]) {
-                        account.changeAddressCount = path[2];
+                    if (path[1] == 0 && account.receiveAddressCount <= path[2]) {
+                        account.receiveAddressCount = path[2]+1;
+                    } else if (path[1] == 1 && account.changeAddressCount <= path[2]) {
+                        account.changeAddressCount = path[2]+1;
                     }
 
                     tx.account_indexes.push(parseInt(j));
@@ -1103,10 +1103,10 @@ var MyWallet = new function() {
                     var path = output.xpub.path.split("/");
                     path[1] = parseInt(path[1]);
                     path[2] = parseInt(path[2]);        
-                    if (path[1] == 0 && account.receiveAddressCount < path[2]) {
-                        account.receiveAddressCount = path[2];
-                    } else if (path[1] == 1 && account.changeAddressCount < path[2]) {
-                        account.changeAddressCount = path[2];
+                    if (path[1] == 0 && account.receiveAddressCount <= path[2]) {
+                        account.receiveAddressCount = path[2]+1;;
+                    } else if (path[1] == 1 && account.changeAddressCount <= path[2]) {
+                        account.changeAddressCount = path[2]+1;;
                     }
 
                     tx.account_indexes.push(parseInt(j));
