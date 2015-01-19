c8b7a7afb75aa2d4a7bc3e9337ec7242160a7e22
Sjors Provoost <sjors@sprovoost.nl>
2015-01-19 11:40:00 +0100

    Add format=plain argument to polling request.

--- wallet.js
+++ wallet.js
@@ -3408,6 +3408,10 @@ var MyWallet = new function() {
 
         $.ajax({
             dataType: 'json',
+            data: {format : 'plain'},
+            // xhrFields: {
+            //     withCredentials: true
+            // },
             type: "GET",
             url: BlockchainAPI.getRootURL() + 'wallet/poll-for-session-guid',
             success: function (obj) {
