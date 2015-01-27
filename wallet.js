c3e3ca69f246900e1fcd93527e271ce29411cd35
Tim Lee <stequald@gmail.com>
2015-01-26 16:52:45 -0800

    allow at symbol

--- wallet.js
+++ wallet.js
@@ -3094,7 +3094,7 @@ var MyWallet = new function() {
     }
 
     function isAlphaNumericSpace(input) {
-        return XRegExp("^\\p{L}[\\p{L} \\-,._']*$").test(input) || /^[\w\-,._  ]+$/.test(input);
+        return XRegExp("^\\p{L}[\\p{L}@ \\-,._']*$").test(input) || /^[\w\-,._  ]+$/.test(input);
     }
         
     function parseMultiAddressJSON(obj, cached, checkCompleted) {
