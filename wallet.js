f6955db6c0e9c42d79626b963a6b28b82ff7fa7a
Tim Lee <stequald@gmail.com>
2015-02-03 18:07:19 -0500

    detect private key type before claiming

--- wallet.js
+++ wallet.js
@@ -2062,7 +2062,13 @@ var MyWallet = new function() {
         try {
             var format = MyWallet.detectPrivateKeyFormat(privatekey);
             var privateKeyToSweep = MyWallet.privateKeyStringToKey(privatekey, format);
-            var from_address = MyWallet.getCompressedAddressString(privateKeyToSweep);
+            
+            var from_address;
+            if (detectPrivateKeyFormat == 'base58' || detectPrivateKeyFormat == 'base64') {
+                from_address = MyWallet.getUnCompressedAddressString(privateKeyToSweep);
+            else {
+                from_address = MyWallet.getCompressedAddressString(privateKeyToSweep);
+            }
 
             BlockchainAPI.get_balance([from_address], function(value) {
 
