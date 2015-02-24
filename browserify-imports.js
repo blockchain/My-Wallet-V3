d6b6d658ade7e913fcd606f9b9bb98fb62899437
Sjors Provoost <sjors@sprovoost.nl>
2015-02-24 09:07:20 -0500

    Ignored the wrong file

---
+++ browserify-imports.js
@@ -0,0 +1,8 @@
+module.exports = {
+  Bitcoin: require('bitcoinjs-lib'),
+  BigInteger: require('bigi'),
+  Buffer: require('buffer'),
+  assert: require('assert'),
+  Base58: require('bs58'),
+  JSONB: require('json-buffer')
+}
