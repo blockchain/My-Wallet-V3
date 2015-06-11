fb64fdb76acb0c99b8a22fe468a3112345b6a771
Jaume Pernas <jaume@pernas.cat>
2015-06-11 11:10:08 +0100

    working on hdwallet.

--- src/hdw.js
+++ src/hdw.js
@@ -33,16 +33,55 @@ Object.defineProperties(HDWallet.prototype, {
   "bip39Password": {
     configurable: false,
     get: function() { return this._bip39Password;}
+  },
+  "mnemonic_verified": {
+    configurable: false,
+    get: function() { return this._mnemonic_verified;}
+  },
+  "default_account_idx": {
+    configurable: false,
+    get: function() { return this._default_account_idx;},
+    set: function(value) {
+      if(this.isValidAccountIndex(value))
+        this._default_account_idx = value;
+      else
+        throw 'Error: unvalid default index account';
+    }
   }
 });
+////////////////////////////////////////////////////////////////////////////////
+// Constructors
 
 HDWallet.example = function(){
+
   var object = {
     seedHex : "f56bf734c9bb4e8d08dd645e9d6adc68",
     bip39Password : ""
   };
   return new HDWallet(object);
 };
+
+// we need 3 actions
+// new hdwallet
+// load hdwallet
+// restore hdwallet
+
+HDWallet.new = function(seedHex, bip39Password){
+
+  assert(Helpers.isString(seedHex), 'hdwallet.seedHex must exist');
+  if (!Helpers.isString(bip39Password)) bip39Password = "";
+  var hdwallet = {
+    seed_hex            : seedHex,
+    passphrase          : bip39Password,
+    mnemonic_verified   : false,
+    default_account_idx : 0,
+    accounts            : []
+  };
+  return new HDWallet(hdwallet);
+};
+
+////////////////////////////////////////////////////////////////////////////////
+// JSON serializer
 
 HDWallet.prototype.toJSON = function(){
 
@@ -54,4 +93,28 @@ HDWallet.prototype.toJSON = function(){
     accounts            : this._accounts
   };
   return hdwallet;
+};
+////////////////////////////////////////////////////////////////////////////////
+// methods
+
+HDWallet.prototype.verifyMnemonic = function(){
+
+  this._mnemonic_verified = true;
+  return this;
+};
+////////////////////////////////////////////////////////////////////////////////
+// account managment
+
+HDWallet.prototype.newAccount = function(label){
+
+// ESTIC AQUI, de moment no implemento second password.
+  return this;
+};
+
+
+////////////////////////////////////////////////////////////////////////////////
+// checkers
+HDWallet.prototype.isValidAccountIndex = function(index){
+
+  return Helpers.isNumber(index) && index >= 0 && index < this._accounts.length;
 };
