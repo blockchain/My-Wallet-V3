3a99d6b9a13f7b3d0c56fe8cf375907d7176856e
Jaume Pernas <jaume@pernas.cat>
2015-07-20 10:40:07 -0400

    Address Book fixed.

--- src/blockchain-wallet.js
+++ src/blockchain-wallet.js
@@ -59,7 +59,11 @@ function Wallet(object) {
   // address book in json is [{address: "address1", label: "label1"} , ... ]
   // address book in memory is {address1: "label1", address2: "label2"}
   this._address_book = obj.address_book ?
-    obj.address_book.reduce(function(o,a){o[a.address] = a.label; return o;}, {}) : {};
+    obj.address_book.reduce(function(o,a){
+                              var address = a.address || a.addr;
+                              o[address] = a.label;
+                              return o;
+                            }, {}) : {};
 
   // tx_notes dictionary
   this._tx_notes = obj.tx_notes || {};
@@ -197,14 +201,7 @@ Object.defineProperties(Wallet.prototype, {
   },
   "addressBook":{
     configurable: false,
-    get: function() {
-      var that = this;
-      return Object.keys(this._address_book)
-               .map(function(a){
-                      return {address: a, label: that.getAddressBookLabel(a)};
-                    }
-                );
-    }
+    get: function() { return this._address_book;}
   },
   "logoutTime":{
     configurable: false,
@@ -221,6 +218,12 @@ Object.defineProperties(Wallet.prototype, {
 });
 
 Wallet.prototype.toJSON = function(){
+
+  function addressBookToJSON (addressBook) {
+    return Object.keys(addressBook)
+             .map(function(a){ return {addr: a, label: addressBook[a]};});
+  };
+
   var wallet = {
     guid              : this.guid,
     sharedKey         : this.sharedKey,
@@ -233,7 +236,7 @@ Wallet.prototype.toJSON = function(){
       html5_notifications      : this._html5_notifications,
       logout_time              : this._logout_time
     },
-    address_book      : this.addressBook,
+    address_book      : addressBookToJSON(this._address_book),
     tx_notes          : this._tx_notes,
     // tx_tags           : this._tx_tags,
     tx_names          : this._tx_names,
