2a8968ba384fe248ba2b9c8c1031ad74b6d3c59e
Jaume Pernas <jaume@pernas.cat>
2015-08-20 00:38:40 -0400

    API must compute time_offset using ntp alg

--- src/api.js
+++ src/api.js
@@ -16,6 +16,7 @@ function API(){
   this.AJAX_TIMEOUT       = 60000;
   this.AJAX_RETRY_DEFAULT = 2;
   this.API_CODE           = "1770d5d9-bcea-4d28-ad21-6cbd5be018a8";
+  this.SERVER_TIME_OFFSET = 0;
 }
 
 // encodeFormData :: Object -> url encoded params
@@ -53,39 +54,6 @@ API.prototype.request = function(action, method, data) {
   return defer.promise;
 };
 
-////////////////////////////////////////////////////////////////////////////////
-// Definition of API
-API.prototype.getBalances = function(addresses){
-  var data = {
-      active : addresses.join('|')
-    , simple: true
-    , format: 'json'
-  };
-  return this.request("POST", "multiaddr", data);
-};
-
-API.prototype.getFiatAtTime = function(time, value, currencyCode){
-  var data = {
-      value : value
-    , currency: currencyCode
-    , time: time
-    , textual: false
-    , nosavecurrency: true
-  };
-  return this.request("GET", "frombtc", data);
-};
-
-API.prototype.getTicker = function(){
-  var data = { format: 'json' };
-  return this.request("GET", "ticker", data);
-};
-
-API.prototype.getRejectionReason = function(hexhash){
-  var data = {format: 'plain'};
-  return this.request("GET", "q/rejected/" + hexhash, data);
-};
-
-
 API.prototype.retry = function(f, n) {
   var self = this;
   var i = n === null || n === undefined ? this.AJAX_RETRY_DEFAULT : n;
@@ -100,6 +68,37 @@ API.prototype.retry = function(f, n) {
   };
 };
 
+////////////////////////////////////////////////////////////////////////////////
+// Definition of API
+API.prototype.getBalances = function(addresses){
+  var data = {
+      active : addresses.join('|')
+    , simple: true
+    , format: 'json'
+  };
+  return this.retry(this.request.bind(this, "POST", "multiaddr", data));
+};
+
+API.prototype.getFiatAtTime = function(time, value, currencyCode){
+  var data = {
+      value : value
+    , currency: currencyCode
+    , time: time
+    , textual: false
+    , nosavecurrency: true
+  };
+  return this.retry(this.request.bind(this, "GET", "frombtc", data));
+};
+
+API.prototype.getTicker = function(){
+  var data = { format: 'json' };
+  return this.retry(this.request.bind(this, "GET", "ticker", data));
+};
+
+API.prototype.getRejectionReason = function(hexhash){
+  var data = {format: 'plain'};
+  return this.retry(this.request.bind(this, "GET", "q/rejected/" + hexhash, data));
+};
 
 API.prototype.getUnspent = function(fromAddresses, confirmations){
   var data = {
@@ -108,10 +107,31 @@ API.prototype.getUnspent = function(fromAddresses, confirmations){
     , format: 'json'
   };
   return this.retry(this.request.bind(this, "POST", "unspent", data));
-  // return this.request("POST", "unspent", data);
 };
 
+API.prototype.getHistory = function (addresses, tx_filter, offset, n) {
 
+  var clientTime = (new Date()).getTime();
+  offset = offset || 0;
+  n = n || 0;
+
+  var data = {
+      active : addresses.join('|')
+    , format: 'json'
+    , offset: offset
+    , no_compact: true
+    , ct : clientTime
+    , n : n
+    , language : WalletStore.getLanguage()
+    , no_buttons : true
+  };
+
+  if (tx_filter != undefined && tx_filter != null) {
+    data.filter = tx_filter;
+  }
+
+  return this.retry(this.request.bind(this, "POST", "multiaddr", data));
+};
 
 // things to do:
 // - create a method in blockchain-wallet that ask for the balances and updates the balance wallet
