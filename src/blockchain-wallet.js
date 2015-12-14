4e66e2bbb66b05b7b834201ffe3ba511ae7d3021
pernas <jaume@pernas.cat>
2015-12-14 11:37:12 -0500

    merge(iOS): cherry picked kevin fixes

--- src/blockchain-wallet.js
+++ src/blockchain-wallet.js
@@ -471,7 +471,7 @@ Wallet.prototype.importLegacyAddress = function(addr, label, secPass, bipPass){
       ad.encrypt(cipher).persist();
     };
     this._addresses[ad.address] = ad;
-    ws.send('{"op":"addr_sub", "addr":"'+ad.address+'"}');
+    MyWallet.ws.send('{"op":"addr_sub", "addr":"'+ad.address+'"}');
     defer.resolve(ad);
     MyWallet.syncWallet();
     this.getHistory();
