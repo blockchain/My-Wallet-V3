c5b113e8d0384df3646afc24505b4173167816b8
Sjors Provoost <sjors@sprovoost.nl>
2015-12-18 14:23:23 +0100

    feat(Endpoint): verify-email-token processes result better

--- src/wallet-token-endpoints.js
+++ src/wallet-token-endpoints.js
@@ -4,7 +4,23 @@ var assert = require('assert');
 
 var API = require('./api');
 
-function verifyEmail(token, success, error) {
+function verifyEmail(token, successCallback, errorCallback) {
+  const success = (res) => {
+    if(res && res.success != undefined) {
+      if(res.success) {
+        successCallback(res.guid);
+      } else {
+        errorCallback(res.error);
+      }
+    } else {
+      errorCallback("VERIFY_EMAIL_ENDPOINT_UNEXPECTED_RESPONSE");
+    }
+  }
+
+  const error = (err) => {
+    errorCallback(err);
+  }
+
   var myData = { token: token,  method : 'verify-email-token', api_code : API.API_CODE};
   API.request("POST", 'wallet', myData, false).then(success).catch(error);
 }
