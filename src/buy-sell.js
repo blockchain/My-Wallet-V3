a77f072c09029c643c4ec590de62bc8ebadb4ba0
Sjors Provoost <sjors@sprovoost.nl>
2016-08-30 10:20:24 +0200

    fix(BuySell): stop if 2nd password is enabled

--- src/buy-sell.js
+++ src/buy-sell.js
@@ -5,6 +5,9 @@ module.exports = BuySell;
 function BuySell (wallet) {
   this._wallet = wallet;
 
+  // Stop if 2nd password is enabled
+  if (wallet.external === null) return;
+
   // Add Coinify if not already added:
   if (!this._wallet.external.coinify) this._wallet.external.addCoinify();
 }
@@ -13,6 +16,7 @@ Object.defineProperties(BuySell.prototype, {
   'exchanges': {
     configurable: false,
     get: function () {
+      if (this._wallet.external === null) return;
       return {
         coinify: this._wallet.external.coinify
       };
