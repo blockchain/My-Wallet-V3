4ce42ca15174f753c702a216babdb87a3dad06b9
plondon <plondon@gm.slc.edu>
2016-09-01 17:50:58 -0400

    feat(Trade): add sendAmount

--- src/coinify/trade.js
+++ src/coinify/trade.js
@@ -67,6 +67,12 @@ Object.defineProperties(CoinifyTrade.prototype, {
       return this._state;
     }
   },
+  'sendAmount': {
+    configurable: false,
+    get: function () {
+      return this._sendAmount;
+    }
+  },
   'outAmountExpected': {
     configurable: false,
     get: function () {
@@ -129,6 +135,7 @@ CoinifyTrade.prototype.set = function (obj) {
     this._outCurrency = obj.outCurrency;
     this._inAmount = obj.inAmount;
     this._medium = obj.transferIn.medium;
+    this._sendAmount = obj.transferIn.sendAmount;
     if (this._medium === 'bank') {
       this._bankAccount = new BankAccount(obj.transferIn.details);
     }
