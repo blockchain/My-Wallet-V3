blockchain/My-Wallet-V3

commit 32789ad78e038252b71fdc29b3cf81fe4044b7dd

fix(metadata):do not create metadata entry on login. (#267)

src/external.js
@@ -48,8 +48,28 @@ External.prototype.fetchOrCreate = function () {
   return this._metadata.fetch().then(createOrPopulate.bind(this)).catch(fetchFailed.bind(this));
 };
 
+External.prototype.fetch = function () {
+  var Populate = function (object) {
+    this.success = true;
+    if (object !== null) {
+      this._coinify = object.coinify ? new Coinify(object.coinify, this, this._delegate) : undefined;
+    }
+    return this;
+  };
+  var fetchFailed = function (e) {
+    // Metadata service is down or unreachable.
+    this.success = false;
+    return Promise.reject(e);
+  };
+  return this._metadata.fetch().then(Populate.bind(this)).catch(fetchFailed.bind(this));
+};
+
 External.prototype.save = function () {
-  return this._metadata.update(this);
+  if (!this._coinify) {
+    return this._metadata.create(this);
+  } else {
+    return this._metadata.update(this);
+  }
 };
 
 External.prototype.wipe = function () {

src/blockchain-wallet.js
@@ -809,6 +809,6 @@ Wallet.prototype.loadExternal = function () {
     return Promise.resolve();
   } else {
     this._external = new External(this);
-    return this._external.fetchOrCreate();
+    return this._external.fetch();
   }
 };

commit 7955ef94d738c4b1f2e67c29913b6597df2e419a

fix(BuySell): continue login if metadata fetch fails (broken in #267)

src/buy-sell.js
@@ -9,7 +9,7 @@ function BuySell (wallet) {
   if (wallet.external === null) return;
 
   // Stop if meta data failed to load;
-  if (!wallet.external.success) {
+  if (!wallet.external.loaded) {
     return;
   }
 
@@ -22,7 +22,7 @@ Object.defineProperties(BuySell.prototype, {
     configurable: false,
     get: function () {
       return {
-        metaDataService: this._wallet.external && this._wallet.external.success
+        metaDataService: this._wallet.external && this._wallet.external.loaded
       };
     }
   },
@@ -31,7 +31,7 @@ Object.defineProperties(BuySell.prototype, {
     get: function () {
       if (
         this._wallet.external === null ||
-        !this._wallet.external.success
+        !this._wallet.external.loaded
       ) return;
       return {
         coinify: this._wallet.external.coinify

src/external.js
@@ -29,28 +29,9 @@ External.prototype.toJSON = function () {
   return external;
 };
 
-External.prototype.fetchOrCreate = function () {
-  var createOrPopulate = function (object) {
-    this.success = true;
-    if (object === null) { // entry non exitent
-      return this._metadata.create(this);
-    } else {
-      this._coinify = object.coinify ? new Coinify(object.coinify, this, this._delegate) : undefined;
-      return this;
-    }
-  };
-  var fetchFailed = function (e) {
-    // Metadata service is down or unreachable.
-    this.success = false;
-    // Login should still resolve
-    return Promise.resolve();
-  };
-  return this._metadata.fetch().then(createOrPopulate.bind(this)).catch(fetchFailed.bind(this));
-};
-
 External.prototype.fetch = function () {
   var Populate = function (object) {
-    this.success = true;
+    this.loaded = true;
     if (object !== null) {
       this._coinify = object.coinify ? new Coinify(object.coinify, this, this._delegate) : undefined;
     }
@@ -58,7 +39,7 @@ External.prototype.fetch = function () {
   };
   var fetchFailed = function (e) {
     // Metadata service is down or unreachable.
-    this.success = false;
+    this.loaded = false;
     return Promise.reject(e);
   };
   return this._metadata.fetch().then(Populate.bind(this)).catch(fetchFailed.bind(this));
@@ -73,7 +54,7 @@ External.prototype.save = function () {
 };
 
 External.prototype.wipe = function () {
-  this._metadata.update({}).then(this.fetchOrCreate.bind(this));
+  this._metadata.update({}).then(this.fetch.bind(this));
   this._coinify = undefined;
 };
 

src/metadata.js
@@ -112,7 +112,7 @@ Metadata.prototype.fetch = function () {
         throw new Error('METADATA_SIGNATURE_VERIFICATION_ERROR');
       }
     }).catch(function (e) {
-      console.log(e);
+      console.error(e);
       return Promise.reject('METADATA_FETCH_FAILED');
     });
   });

src/wallet.js
@@ -346,13 +346,16 @@ MyWallet.initializeWallet = function (pw, decryptSuccess, buildHdSuccess) {
       , buildHdSuccess
     );
   };
-  // load metadata buy-sell
 
-  var loadExternal = function () {
-    return MyWallet.wallet.loadExternal.bind(MyWallet.wallet)();
+  // Attempt to load metadata for buy-sell
+  var tryLoadExternal = function () {
+    var loadExternalFailed = function (message) {
+      console.warn('wallet.external not set:', message);
+    };
+    return MyWallet.wallet.loadExternal.bind(MyWallet.wallet)().catch(loadExternalFailed);
   };
 
-  return Promise.resolve().then(doInitialize).then(loadExternal);
+  return Promise.resolve().then(doInitialize).then(tryLoadExternal);
 };
 
 // used on iOS

tests/external_spec.js.coffee
@@ -48,7 +48,7 @@ describe "External", ->
 
       it "should include partners if present", (done) ->
         e = new External()
-        promise = e.fetchOrCreate().then((res) ->
+        promise = e.fetch().then((res) ->
           expect(e._coinify).toBeDefined()
         )
         expect(promise).toBeResolved(done)
@@ -56,7 +56,7 @@ describe "External", ->
       it "should not cointain any partner by default", (done) ->
         mockPayload = {}
         e = new External()
-        promise = e.fetchOrCreate().then((res) ->
+        promise = e.fetch().then((res) ->
           expect(e._coinify).toBeUndefined()
         )
         expect(promise).toBeResolved(done)
@@ -64,7 +64,7 @@ describe "External", ->
       it 'should not deserialize non-expected fields', (done) ->
         mockPayload = {coinify: {}, rarefield: "I am an intruder"}
         e = new External()
-        promise = e.fetchOrCreate().then((res) ->
+        promise = e.fetch().then((res) ->
           expect(e._coinify).toBeDefined()
           expect(e._rarefield).toBeUndefined()
         )