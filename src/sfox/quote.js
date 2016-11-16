8602b28b3841bc46306c217e6304a509fbb7c290
Justin Tormey <justin@blockchain.com>
2016-11-16 11:14:06 -0500

    refactor(SFOX): improve clarity of quote vs base amount in quote class

--- src/sfox/quote.js
+++ src/sfox/quote.js
@@ -2,26 +2,27 @@ var Exchange = require('bitcoin-exchange-client');
 var PaymentMethod = require('./payment-medium');
 var Trade = require('./trade');
 
-// I use Coinify's convention for the meaning of base & quote currency.
+var { toCents, toSatoshi } = Exchange.Helpers;
+var isBTC = (c) => c === 'BTC';
+var flipCurrency = (c) => isBTC(c) ? 'USD' : 'BTC';
+
 class Quote extends Exchange.Quote {
-  constructor (obj, amountCurrency, api, delegate, debug) {
+  constructor (obj, baseCurrency, api, delegate, debug) {
     super(api, delegate, Trade, PaymentMethod, debug);
 
     var expiresAt = new Date(obj.expires_on);
+    var btcAmount = toSatoshi(obj.base_amount);
+    var usdAmount = toCents(obj.quote_amount);
 
     this._id = obj.quote_id;
-    this._baseCurrency = amountCurrency.toUpperCase();
-    if (this._baseCurrency === 'USD') {
-      this._quoteCurrency = 'BTC';
-      this._baseAmount = Math.round(obj.quote_amount * 100);
-      this._quoteAmount = Math.round(obj.base_amount * 100000000);
-    } else {
-      this._quoteCurrency = 'USD';
-      this._baseAmount = Math.round(obj.base_amount * 100000000);
-      this._quoteAmount = Math.round(obj.quote_amount * 100);
-    }
-    this._rate = (obj.quote_amount / obj.base_amount).toFixed(2);
     this._expiresAt = expiresAt;
+    this._rate = (obj.quote_amount / obj.base_amount).toFixed(2);
+
+    this._baseCurrency = baseCurrency.toUpperCase();
+    this._baseAmount = isBTC(this._baseCurrency) ? btcAmount : usdAmount;
+
+    this._quoteCurrency = flipCurrency(this._baseCurrency);
+    this._quoteAmount = isBTC(this._quoteCurrency) ? btcAmount : usdAmount;
   }
 
   get rate () {
@@ -36,21 +37,19 @@ class Quote extends Exchange.Quote {
     };
 
     const getQuote = (_baseAmount) => {
-      var getQuote = function () {
-        return api.POST('quote/', {
-          action: 'buy',
-          base_currency: 'btc',
-          quote_currency: 'usd',
-          amount: _baseAmount,
-          amount_currency: baseCurrency.toLowerCase()
-        }, 'v1', 'quotes');
-      };
-
-      return getQuote().then(processQuote);
+      return api.POST('quote/', {
+        action: 'buy',
+        base_currency: 'btc',
+        quote_currency: 'usd',
+        amount: _baseAmount,
+        amount_currency: baseCurrency.toLowerCase()
+      }, 'v1', 'quotes');
     };
 
-    return super.getQuote(-amount, baseCurrency, quoteCurrency, ['BTC', 'EUR', 'GBP', 'USD', 'DKK'], debug)
-             .then(getQuote);
+    return super
+      .getQuote(-amount, baseCurrency, quoteCurrency, ['BTC', 'EUR', 'GBP', 'USD', 'DKK'], debug)
+      .then(getQuote)
+      .then(processQuote);
   }
 }
 
