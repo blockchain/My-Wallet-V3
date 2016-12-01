8859e98674c1a01cfb5d81a5eb53f3f399791384
Justin Tormey <justin@blockchain.com>
2016-12-01 14:04:15 -0500

    fix(Metadata): protect _value from unexpected mutations

--- src/metadata.js
+++ src/metadata.js
@@ -120,9 +120,12 @@ Metadata.extractResponse = R.curry((encKey, res) => {
   }
 });
 
+Metadata.toImmutable = R.compose(Object.freeze, JSON.parse, JSON.stringify);
+
 Metadata.prototype.create = function (payload) {
+  const M = Metadata;
+  payload = M.toImmutable(payload);
   return this.next(() => {
-    const M = Metadata;
     const encPayloadBuffer = this._encKeyBuffer
       ? R.compose(M.B64ToBuffer, M.encrypt(this._encKeyBuffer), JSON.stringify)(payload)
       : R.compose(M.StringToBuffer, JSON.stringify)(payload);
@@ -146,7 +149,7 @@ Metadata.prototype.create = function (payload) {
 
 Metadata.prototype.update = function (payload) {
   if (JSON.stringify(payload) === JSON.stringify(this._value)) {
-    return this.next(() => Promise.resolve(payload));
+    return this.next(() => Promise.resolve(Metadata.toImmutable(payload)));
   } else {
     return this.create(payload);
   }
@@ -162,7 +165,7 @@ Metadata.prototype.fetch = function () {
     };
     const saveValue = (res) => {
       if (res === null) return res;
-      this._value = res;
+      this._value = Metadata.toImmutable(res);
       return res;
     };
     return M.GET(this._address).then(M.verifyResponse(this._address))
