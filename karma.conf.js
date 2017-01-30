45e8f3f9dc532d3664100a78a6d8e43b9da85dab
Justin Tormey <justin@blockchain.com>
2017-01-29 22:33:02 -0500

    chore(Decaf): metadata_spec.js

--- karma.conf.js
+++ karma.conf.js
@@ -99,7 +99,7 @@ module.exports = function (config) {
       'tests/wallet_signup_spec.js.coffee',
       'tests/blockchain_settings_api_spec.js',
       'tests/account_info_spec.js',
-      'tests/metadata_spec.js.coffee',
+      'tests/metadata_spec.js',
       'tests/exchange_delegate_spec.js'
     ]
   };
