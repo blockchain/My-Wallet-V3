948bc4dcf35b6ba13dbd8113ad490f50528a4110
Justin Tormey <justin@blockchain.com>
2017-07-18 13:28:03 -0400

    feat(Eth): api methods for recording usage stats

--- src/api.js
+++ src/api.js
@@ -297,6 +297,20 @@ API.prototype.incrementLoginViaQrStats = function () {
   return fetch(this.ROOT_URL + 'event?name=wallet_web_login_via_qr');
 };
 
+API.prototype.btcEthUsageStats = function (btcBalance, ethBalance, btcTxs, ethTxs) {
+  let base = 'event?web_wallet_btc_eth_usage_';
+  let counterId = this.calcBtcEthUsageCounterId(btcBalance, ethBalance, btcTxs, ethTxs);
+  return fetch(this.ROOT_URL + base + counterId);
+};
+
 API.prototype.getBlockchainAddress = function () {
   return this.request('GET', 'charge_address');
+};
+
+API.prototype.calcBtcEthUsageCounterId = function (btcBalance, ethBalance, btcTxsCount, ethTxsCount) {
+  // computes a number in the range 0..15 (represents each possible state given 4 binary options)
+  // bits 0, 1, 2, and 3, respectively:
+  return [ethTxsCount, btcTxsCount, ethBalance, btcBalance]
+    .map(field => field === 0 ? 0 : 1)
+    .reduce((acc, bit, index) => acc | (bit << index));
 };
--- tests/api_spec.js
+++ tests/api_spec.js
@@ -16,4 +16,38 @@ describe('API', () => {
       expect(API.encodeFormData(data)).toEqual('...');
     });
   });
+
+  describe('calcBtcEthUsageCounterId', () => {
+    // values: BTC Balance, ETH Balance, BTC Txs, Eth Txs
+    const fixtures = [
+      { id: 0, values: [0, 0, 0, 0] },
+      { id: 1, values: [0, 0, 0, 1] },
+      { id: 2, values: [0, 0, 1, 0] },
+      { id: 3, values: [0, 0, 1, 1] },
+      { id: 4, values: [0, 1, 0, 0] },
+      { id: 5, values: [0, 1, 0, 1] },
+      { id: 6, values: [0, 1, 1, 0] },
+      { id: 7, values: [0, 1, 1, 1] },
+      { id: 8, values: [1, 0, 0, 0] },
+      { id: 9, values: [1, 0, 0, 1] },
+      { id: 10, values: [1, 0, 1, 0] },
+      { id: 11, values: [1, 0, 1, 1] },
+      { id: 12, values: [1, 1, 0, 0] },
+      { id: 13, values: [1, 1, 0, 1] },
+      { id: 14, values: [1, 1, 1, 0] },
+      { id: 15, values: [1, 1, 1, 1] }
+    ];
+
+    let symbol = (val) => val === 0 ? '== 0' : '> 0';
+
+    let renderValues = ([btcBal, ethBal, btcTxs, ethTxs]) => (
+      `BTC Balance ${symbol(btcBal)}, ETH Balance ${symbol(ethBal)}, BTC Txs ${symbol(btcTxs)}, and ETH Txs ${symbol(ethTxs)}`
+    );
+
+    fixtures.forEach(({ id, values }) => {
+      it(`should produce counter id = ${id} with ${renderValues(values)}`, () => {
+        expect(API.calcBtcEthUsageCounterId.apply(API, values)).toEqual(id);
+      });
+    });
+  });
 });
