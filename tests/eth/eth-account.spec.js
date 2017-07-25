a58e5f4a295f1acf4edac51332fe39ce309c0530
Justin Tormey <justin@blockchain.com>
2017-07-25 14:14:37 -0400

    test(Eth): add tests for eth-account

---
+++ tests/eth/eth-account.spec.js
@@ -0,0 +1,145 @@
+/* eslint-disable semi */
+const EthAccount = require('../../src/eth/eth-account')
+const EthTxBuilder = require('../../src/eth/eth-tx-builder')
+const EthWalletTx = require('../../src/eth/eth-wallet-tx')
+
+describe('EthAccount', () => {
+  const wallet = {
+    getPrivateKey () {
+      return Buffer.from('6858f113ba3bf55880105726c0d9f0495756321b45f821bc228fca2adacbb87b', 'hex')
+    }
+  }
+
+  describe('instance', () => {
+    let account
+    beforeEach(() => {
+      account = EthAccount.fromWallet(wallet)
+      account.label = 'Test Account'
+    })
+
+    describe('getters', () => {
+      it('should have: address', () => {
+        expect(account.address).toEqual('0xd70073f72621fb90060ac257f38cf2ff566ea6bb')
+      })
+
+      it('should have: privateKey', () => {
+        let accountWithPriv = new EthAccount({ priv: wallet.getPrivateKey() })
+        expect(accountWithPriv.privateKey.toString('hex')).toEqual('6858f113ba3bf55880105726c0d9f0495756321b45f821bc228fca2adacbb87b')
+      })
+
+      it('should have: wei', () => {
+        expect(account.wei).toEqual(0)
+      })
+
+      it('should have: balance', () => {
+        expect(account.balance).toEqual('0')
+      })
+
+      it('should have: txs', () => {
+        expect(account.txs).toEqual([])
+      })
+
+      it('should have: nonce', () => {
+        expect(account.nonce).toEqual(0)
+      })
+
+      it('should have: label', () => {
+        expect(account.label).toEqual('Test Account')
+      })
+
+      it('should have: archived', () => {
+        expect(account.archived).toEqual(false)
+      })
+    })
+
+    describe('.createPayment()', () => {
+      it('should create a new EthTxBuilder', () => {
+        let payment = account.createPayment()
+        expect(payment.constructor).toEqual(EthTxBuilder)
+      })
+    })
+
+    describe('.incrementNonce()', () => {
+      it('should increment the nonce', () => {
+        account.incrementNonce()
+        expect(account.nonce).toEqual(1)
+      })
+    })
+
+    describe('.setData()', () => {
+      it('should set the account balance', () => {
+        account.setData({ balance: '10000000000000000' })
+        expect(account.balance).toEqual('0.01')
+      })
+
+      it('should set the account nonce', () => {
+        account.setData({ nonce: 10 })
+        expect(account.nonce).toEqual(10)
+      })
+
+      it('should not set a nonce lower than the current one', () => {
+        account.setData({ nonce: -1 })
+        expect(account.nonce).toEqual(0)
+      })
+    })
+
+    describe('.setTransactions()', () => {
+      it('should set the account transactions', () => {
+        account.setTransactions({ txns: [{ hash: 'adsf' }] })
+        expect(account.txs.length).toEqual(1)
+        expect(account.txs[0].constructor).toEqual(EthWalletTx)
+      })
+    })
+
+    describe('.updateTxs()', () => {
+      it('should update each tx', () => {
+        let ethWallet = { latestBlock: 125, getTxNote () {} }
+        account.setTransactions({ txns: [{ hash: 'adsf' }] })
+        spyOn(account.txs[0], 'update')
+        account.updateTxs(ethWallet)
+        expect(account.txs[0].update).toHaveBeenCalledWith(ethWallet)
+      })
+    })
+
+    describe('.toJSON()', () => {
+      it('should serialize to json', () => {
+        let expected = '{"label":"Test Account","archived":false,"addr":"0xd70073f72621fb90060ac257f38cf2ff566ea6bb"}'
+        expect(JSON.stringify(account)).toEqual(expected)
+      })
+    })
+
+    describe('.isCorrectPrivateKey()', () => {
+      it('should identify a correct private key', () => {
+        let correct = wallet.getPrivateKey()
+        expect(account.isCorrectPrivateKey(correct)).toEqual(true)
+      })
+
+      it('should identify an incorrect private key', () => {
+        let incorrect = Buffer.from('6034edd08a4153c66e653829bdfcd2bcb3c49a16f2d10e4023676a9574879647', 'hex')
+        expect(account.isCorrectPrivateKey(incorrect)).toEqual(false)
+      })
+    })
+  })
+
+  describe('static', () => {
+    describe('.defaultLabel()', () => {
+      it('should create a label', () => {
+        expect(EthAccount.defaultLabel()).toEqual('My Ether Wallet')
+      })
+
+      it('should create a label for account at index 1', () => {
+        expect(EthAccount.defaultLabel(1)).toEqual('My Ether Wallet 2')
+      })
+    })
+
+    describe('.fromWallet()', () => {
+      it('should create an EthAccount instance', () => {
+        let account = EthAccount.fromWallet(wallet)
+        expect(account.constructor).toEqual(EthAccount)
+        expect(account.balance).toEqual('0')
+        expect(account.nonce).toEqual(0)
+        expect(account.address).toEqual('0xd70073f72621fb90060ac257f38cf2ff566ea6bb')
+      })
+    })
+  })
+})
