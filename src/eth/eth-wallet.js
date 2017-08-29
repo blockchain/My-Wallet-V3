4cdf568f3cca6eb09222ed999c8d152925c6f157
Justin Tormey <justin@blockchain.com>
2017-08-29 13:13:56 -0400

    feat(Eth): batch fetch eth account data

--- src/eth/eth-wallet.js
+++ src/eth/eth-wallet.js
@@ -195,18 +195,25 @@ class EthWallet {
   }
 
   fetchHistory () {
-    return Promise.all(this.activeAccountsWithLegacy.map(a => a.fetchHistory()))
+    return Promise.all([this.fetchBalance(), this.fetchTransactions()])
       .then(() => this.updateTxs())
       .then(() => this.getLatestBlock());
   }
 
   fetchBalance () {
-    return Promise.all(this.activeAccountsWithLegacy.map(a => a.fetchBalance()));
+    let accounts = this.activeAccountsWithLegacy;
+    let addresses = accounts.map(a => a.address);
+    return fetch(`${API.API_ROOT_URL}eth/account/${addresses.join()}/balance`)
+      .then(r => r.status === 200 ? r.json() : r.json().then(e => Promise.reject(e)))
+      .then(data => accounts.forEach(a => a.setData(data[a.address])));
   }
 
   fetchTransactions () {
-    return Promise.all(this.activeAccountsWithLegacy.map(a => a.fetchTransactions()))
-      .then(() => this.updateTxs());
+    let accounts = this.activeAccountsWithLegacy;
+    let addresses = accounts.map(a => a.address);
+    return fetch(`${API.API_ROOT_URL}eth/account/${addresses.join()}`)
+      .then(r => r.status === 200 ? r.json() : r.json().then(e => Promise.reject(e)))
+      .then(data => accounts.forEach(a => a.setTransactions(data[a.address])));
   }
 
   fetchFees () {
