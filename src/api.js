1b0d32e90f1aebf7075c60cdf543dd033f87ae36
Philip Welber <welber@blockchain.com>
2017-09-15 11:07:50 -0400

    feat(API): change endpoint name

--- src/api.js
+++ src/api.js
@@ -315,7 +315,7 @@ API.prototype.incrementBtcEthUsageStats = function (btcBalance, ethBalance) {
 };
 
 API.prototype.getPriceChartData = function (params) {
-  return this.requestApi(`historicalprices?base=${params.base}&quote=${params.quote}&start=${params.start}&scale=${params.scale}`);
+  return this.requestApi(`price/index-series?base=${params.base}&quote=${params.quote}&start=${params.start}&scale=${params.scale}`);
 };
 
 API.prototype.incrementBuyLimitCounter = function (amount) {
