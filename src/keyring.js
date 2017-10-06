e85bf57f0faaf09084bd1e9089f94c6f1257af50
Justin Tormey <justin@blockchain.com>
2017-10-06 11:39:48 -0400

    chore(Cash): default to bitcoinjs-lib in KeyRing

--- src/keyring.js
+++ src/keyring.js
@@ -3,12 +3,13 @@
 module.exports = KeyRing;
 
 var assert = require('assert');
+var Bitcoin = require('bitcoinjs-lib');
 var KeyChain = require('./keychain');
 
 // keyring: A collection of keychains
 
 function KeyRing (extendedKey, cache, bitcoinjs) {
-  this._bitcoinjs = bitcoinjs;
+  this._bitcoinjs = bitcoinjs || Bitcoin;
   this._receiveChain = null;
   this._changeChain = null;
   this.init(extendedKey, cache);
