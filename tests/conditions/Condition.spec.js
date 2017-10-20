3eac98b323d8080ad7ecfeddaaf21576b51f5c64
Justin Tormey <justin@blockchain.com>
2017-10-20 14:23:00 -0400

    test(Condition): additional condition tests

--- tests/conditions/Condition.spec.js
+++ tests/conditions/Condition.spec.js
@@ -2,15 +2,8 @@
 const Condition = require('../../src/conditions/Condition');
 
 describe('Condition', () => {
-  const passed = Condition.of(() => ({
-    passed: true,
-    reason: ['this is a passed condition']
-  }))
-
-  const failed = Condition.of(() => ({
-    passed: false,
-    reason: ['this is a failed condition']
-  }))
+  const passed = Condition.of(() => ({ passed: true, reason: ['cond_passing'] }))
+  const failed = Condition.of(() => ({ passed: false, reason: ['cond_failing'] }))
 
   describe('#empty', () => {
     it('should create an empty condition', () => {
@@ -21,25 +14,34 @@ describe('Condition', () => {
 
   describe('#of', () => {
     it('should create a new condition', () => {
-      let c = Condition.of(() => ({
-        passed: false,
-        reason: ['just because']
-      }))
-      expect(c.test()).toEqual({ passed: false, reason: ['just because'] })
+      let c = Condition.of(() => ({ passed: false, reason: ['reason'] }))
+      expect(c.test()).toEqual({ passed: false, reason: ['reason'] })
     })
   })
 
   describe('.and', () => {
     it('should create a composite condition', () => {
       let c = Condition.empty().and(failed)
-      expect(c.test()).toEqual({ passed: false, reason: ['this is a failed condition'] })
+      expect(c.test()).toEqual(failed.test())
+    })
+
+    it('should include all reasons for a passing condition', () => {
+      let c = passed.and(passed)
+      expect(c.test()).toEqual({ passed: true, reason: ['cond_passing', 'cond_passing'] })
+    })
+
+    it('should short-circuit on a failed first condition', () => {
+      let spy = jasmine.createSpy('test cond')
+      let c = failed.and(Condition.of(() => spy()))
+      expect(c.test()).toEqual(failed.test())
+      expect(spy).not.toHaveBeenCalled()
     })
   })
 
   describe('.andNot', () => {
     it('should create a composite negated condition', () => {
       let c = Condition.empty().andNot(passed)
-      expect(c.test()).toEqual({ passed: false, reason: ['this is a passed condition'] })
+      expect(c.test()).toEqual({ passed: false, reason: ['cond_passing'] })
     })
   })
 
@@ -47,25 +49,32 @@ describe('Condition', () => {
     it('should create a passing condition if one branch passes', () => {
       let c1 = passed.or(failed)
       let c2 = failed.or(passed)
-      expect(c1.test()).toEqual({ passed: true, reason: ['this is a passed condition'] })
-      expect(c2.test()).toEqual({ passed: true, reason: ['this is a passed condition'] })
+      expect(c1.test()).toEqual(passed.test())
+      expect(c2.test()).toEqual(passed.test())
     })
 
     it('should create a failing condition if both branches fail', () => {
       let c = failed.or(failed)
-      expect(c.test()).toEqual({ passed: false, reason: ['this is a failed condition'] })
+      expect(c.test()).toEqual(failed.test())
+    })
+
+    it('should short-circuit on a passed first condition', () => {
+      let spy = jasmine.createSpy('test cond')
+      let c = passed.or(Condition.of(() => spy()))
+      expect(c.test()).toEqual(passed.test())
+      expect(spy).not.toHaveBeenCalled()
     })
   })
 
   describe('.negated', () => {
     it('should negate a passing condition', () => {
       let c = passed.negated()
-      expect(c.test()).toEqual({ passed: false, reason: ['this is a passed condition'] })
+      expect(c.test()).toEqual({ passed: false, reason: ['cond_passing'] })
     })
 
     it('should negate a failing condition', () => {
       let c = failed.negated()
-      expect(c.test()).toEqual({ passed: true, reason: ['this is a failed condition'] })
+      expect(c.test()).toEqual({ passed: true, reason: ['cond_failing'] })
     })
   })
 })
