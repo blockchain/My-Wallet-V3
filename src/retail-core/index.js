27910ed246daedc72276169c7c4777d9f4a0a92d
woudini <10579422+woudini@users.noreply.github.com>
2018-08-09 16:00:08 -0400

    feat(Metadata): add retail core entry to metadata

---
+++ src/retail-core/index.js
@@ -0,0 +1,52 @@
+
+/* eslint-disable semi */
+const METADATA_TYPE_RETAIL_CORE = 10;
+
+class RetailCore {
+  constructor (wallet, metadata) {
+    this._wallet = wallet
+    this._metadata = metadata
+    this._userId = null
+    this._token = null
+  }
+
+  get userId () {
+    return this._userId
+  }
+
+  get token () {
+    return this._token
+  }
+
+  updateUserCredentials (credentials) {
+    this._userId = credentials.userId;
+    this._token = credentials.token;
+    this.sync()
+  }
+
+  fetch () {
+    return this._metadata.fetch().then((data) => {
+      this._userId = data ? data.userId : null;
+      this._token = data ? data.token : null;
+    });
+  }
+
+  sync () {
+    return this._metadata.update(this);
+  }
+
+  toJSON () {
+    return {
+      userId: this.userId,
+      has_seen: this.token
+    }
+  }
+
+  static fromBlockchainWallet (wallet) {
+    let metadata = wallet.metadata(METADATA_TYPE_RETAIL_CORE)
+    return new RetailCore(wallet, metadata)
+  }
+}
+
+module.exports = RetailCore
+
