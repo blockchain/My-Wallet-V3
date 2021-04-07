fe14875e219dfcdd7bf768012f1bea109dde5186
Pavel Kiselyov <pavel@blockchain.com>
2021-04-07 16:25:19 +0100

    feat(Segwit): Fee estimation test coverage

--- tests/coin-selection.spec.js
+++ tests/coin-selection.spec.js
@@ -4,12 +4,200 @@ let cs = require('../src/coin-selection')
 let Coin = require('../src/coin')
 
 describe('Coin Selection', () => {
-  describe('byte sizes', () => {
+  const legacyInput = { type: () => 'P2PKH' }
+  const legacyOutput = { type: () => 'P2PKH' }
+  const segwitInput = { type: () => 'P2WPKH' }
+  const segwitOutput = { type: () => 'P2WPKH' }
+
+  describe('0x0 transactions', () => {
     it('should return the right transaction size (empty tx)', () => {
+      // No witness => 10 vbytes
       expect(cs.transactionBytes([], [])).toEqual(10)
     })
-    it('should return the right transaction size (1 in 2 out tx)', () => {
-      expect(cs.transactionBytes([{}], [{}, {}])).toEqual(225)
+  })
+
+  describe('1x1 transactions', () => {
+    it('should return the right transaction size (1 P2PKH, 1 P2PKH)', () => {
+      // 10 + 148 + 34 = 192
+      expect(cs.transactionBytes([legacyInput], [legacyOutput])).toEqual(192)
+    })
+    it('should return the right transaction size (1 P2PKH, 1 P2WPKH)', () => {
+      // 10 + 148 + 31 = 189
+      expect(cs.transactionBytes([legacyInput], [segwitOutput])).toEqual(189)
+    })
+    it('should return the right transaction size (1 P2WPKH, 1 P2PKH)', () => {
+      // 10.75 + 67.75 + 34 = 112.5
+      expect(cs.transactionBytes([segwitInput], [legacyOutput])).toEqual(
+        112.5
+      )
+    })
+    it('should return the right transaction size (1 P2WPKH, 1 P2WPKH)', () => {
+      // 10.75 + 67.75 + 31 = 109.5
+      expect(cs.transactionBytes([segwitInput], [segwitOutput])).toEqual(
+        109.5
+      )
+    })
+  })
+
+  describe('1x2 transactions', () => {
+    it('should return the right transaction size (1 P2PKH, 2 P2PKH)', () => {
+      // 10 + 148 + 34*2 = 226
+      expect(
+        cs.transactionBytes([legacyInput], [legacyOutput, legacyOutput])
+      ).toEqual(226)
+    })
+    it('should return the right transaction size (1 P2PKH, 2 P2WPKH)', () => {
+      // 10 + 148 + 31*2 = 220
+      expect(
+        cs.transactionBytes([legacyInput], [segwitOutput, segwitOutput])
+      ).toEqual(220)
+    })
+    it('should return the right transaction size (1 P2PKH, 1 P2PKH + 1 P2WPKH)', () => {
+      // 10 + 148 + 31 + 34 = 223
+      expect(
+        cs.transactionBytes([legacyInput], [legacyOutput, segwitOutput])
+      ).toEqual(223)
+    })
+    it('should return the right transaction size (1 P2WPKH, 2 P2PKH)', () => {
+      // 10.75 + 67.75 + 34*2 = 146.5
+      expect(
+        cs.transactionBytes([segwitInput], [legacyOutput, legacyOutput])
+      ).toEqual(146.5)
+    })
+    it('should return the right transaction size (1 P2WPKH, 2 P2WPKH)', () => {
+      // 10.75 + 67.75 + 31*2 = 140.5
+      expect(
+        cs.transactionBytes([segwitInput], [segwitOutput, segwitOutput])
+      ).toEqual(140.5)
+    })
+    it('should return the right transaction size (1 P2WPKH, 1 P2PKH + 1 P2WPKH)', () => {
+      // 10.75 + 67.75 + 31 + 34 = 143.5
+      expect(
+        cs.transactionBytes([segwitInput], [legacyOutput, segwitOutput])
+      ).toEqual(143.5)
+    })
+  })
+
+  describe('2x1 transactions', () => {
+    it('should return the right transaction size (2 P2PKH, 1 P2PKH)', () => {
+      // 10 + 148*2 + 34 = 340
+      expect(
+        cs.transactionBytes([legacyInput, legacyInput], [legacyOutput])
+      ).toEqual(340)
+    })
+    it('should return the right transaction size (2 P2PKH, 1 P2WPKH)', () => {
+      // 10 + 148*2 + 31 = 337
+      expect(
+        cs.transactionBytes([legacyInput, legacyInput], [segwitOutput])
+      ).toEqual(337)
+    })
+    it('should return the right transaction size (1 P2PKH + P2WPKH, 1 P2PKH)', () => {
+      // 10.75 + 67.75 + 148 + 34 = 260.5
+      expect(
+        cs.transactionBytes([legacyInput, segwitInput], [legacyOutput])
+      ).toEqual(260.5)
+    })
+    it('should return the right transaction size (2 P2WPKH, 1 P2PKH)', () => {
+      // 10.75 + 67.75*2 + 34 = 180.25
+      expect(
+        cs.transactionBytes([segwitInput, segwitInput], [legacyOutput])
+      ).toEqual(180.25)
+    })
+    it('should return the right transaction size (2 P2WPKH, 1 P2WPKH)', () => {
+      // 10.75 + 67.75*2 + 31 = 177.25
+      expect(
+        cs.transactionBytes([segwitInput, segwitInput], [segwitOutput])
+      ).toEqual(177.25)
+    })
+    it('should return the right transaction size (1 P2PKH + 1 P2WPKH, 1 P2WPKH)', () => {
+      // 10.75 + 67.75 + 148 + 31 = 257.5
+      expect(
+        cs.transactionBytes([legacyInput, segwitInput], [segwitOutput])
+      ).toEqual(257.5)
+    })
+  })
+
+  describe('2x2 transactions', () => {
+    it('should return the right transaction size (2 P2PKH, 2 P2PKH)', () => {
+      // 10 + 148*2 + 34*2 = 374
+      expect(
+        cs.transactionBytes(
+          [legacyInput, legacyInput],
+          [legacyOutput, legacyOutput]
+        )
+      ).toEqual(374)
+    })
+    it('should return the right transaction size (2 P2PKH, 2 P2WPKH)', () => {
+      // 10 + 148*2 + 31*2 = 368
+      expect(
+        cs.transactionBytes(
+          [legacyInput, legacyInput],
+          [segwitOutput, segwitOutput]
+        )
+      ).toEqual(368)
+    })
+    it('should return the right transaction size (1 P2PKH + 1 P2WPKH, 2 P2PKH)', () => {
+      // 10.75 + 148 + 67.75 + 34*2 = 294.5
+      expect(
+        cs.transactionBytes(
+          [legacyInput, segwitInput],
+          [legacyOutput, legacyOutput]
+        )
+      ).toEqual(294.5)
+    })
+    it('should return the right transaction size (2 P2PKH, 1 P2PKH + 1 P2WPKH)', () => {
+      // 10 + 148*2 + 31 + 34 = 371
+      expect(
+        cs.transactionBytes(
+          [legacyInput, legacyInput],
+          [legacyOutput, segwitOutput]
+        )
+      ).toEqual(371)
+    })
+    it('should return the right transaction size (1 P2PKH + 1 P2PWKH, 1 P2PKH + 1 P2WPKH)', () => {
+      // 10.75 + 67.75 + 148 + 31 + 34 = 291.5
+      expect(
+        cs.transactionBytes(
+          [legacyInput, segwitInput],
+          [legacyOutput, segwitOutput]
+        )
+      ).toEqual(291.5)
+    })
+    it('should return the right transaction size (2 P2WPKH, 2 P2PKH)', () => {
+      // 10.75 + 67.75*2 + 34*2 = 214.25
+      expect(
+        cs.transactionBytes(
+          [segwitInput, segwitInput],
+          [legacyOutput, legacyOutput]
+        )
+      ).toEqual(214.25)
+    })
+    it('should return the right transaction size (2 P2WPKH, 2 P2WPKH)', () => {
+      // 10.75 + 67.75*2 + 31*2 = 208.25
+      expect(
+        cs.transactionBytes(
+          [segwitInput, segwitInput],
+          [segwitOutput, segwitOutput]
+        )
+      ).toEqual(208.25)
+    })
+    it('should return the right transaction size (2 P2WPKH, 1 P2PKH + 1 P2WPKH)', () => {
+      // 10.75 + 67.75*2 + 31 + 34 = 211.25
+      expect(
+        cs.transactionBytes(
+          [segwitInput, segwitInput],
+          [legacyOutput, segwitOutput]
+        )
+      ).toEqual(211.25)
+    })
+    it('should return the right transaction size (1 P2PKH + 1 P2WPKH, 2 P2WPKH)', () => {
+      // 10.75 + 67.75 + 148 + 31*2 = 288.5
+      expect(
+        cs.transactionBytes(
+          [legacyInput, segwitInput],
+          [segwitOutput, segwitOutput]
+        )
+      ).toEqual(288.5)
     })
   })
 
