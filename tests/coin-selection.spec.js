4ed22f94bfbd0021830426c99859c2c5f73b63b3
Pavel Kiselyov <pavel@blockchain.com>
2021-04-07 16:36:36 +0100

    fix(Segwit): port effective balances tests

--- tests/coin-selection.spec.js
+++ tests/coin-selection.spec.js
@@ -207,10 +207,18 @@ describe('Coin Selection', () => {
       let outputs = map(Coin.of, [0, 0])
       expect(cs.effectiveBalance(0, inputs, outputs).value).toEqual(45000)
     })
-    it('should return the right effective max Balance', () => {
-      let inputs = map(Coin.of, [15000, 10000, 20000])
-      let outputs = map(Coin.of, [0, 0])
-      expect(cs.effectiveBalance(55, inputs, outputs).value).toEqual(16455)
+    it('should return the right effective max Balance with value and empty valued outputs', () => {
+      const inputs = map(Coin.fromJS, [
+        { value: 15000 },
+        { value: 10000 },
+        { value: 20000 }
+      ])
+
+      const outputs = map(Coin.fromJS, [{ value: 0 }, { value: 0 }])
+
+      // sum of inputs - transactionBytes * feePerByte
+      // 45000 - 55 * (10 + 3*148 + 2*34) = 45000 - 28710 = 16290
+      expect(cs.effectiveBalance(55, inputs, outputs).value).toEqual(16290)
     })
     it('should return the right effective max Balance', () => {
       expect(cs.effectiveBalance(55, [], []).value).toEqual(0)
@@ -221,41 +229,70 @@ describe('Coin Selection', () => {
   })
 
   describe('findTarget', () => {
-    it('should return the right selection', () => {
-      let selection = cs.findTarget([], 0, [])
+    it('should return the right selection with empty inputs and targets, ', () => {
+      const selection = cs.findTarget([], 0, [])
       expect(selection.fee).toEqual(0)
       expect(selection.inputs).toEqual([])
       expect(selection.outputs).toEqual([])
     })
-    it('should return the right selection', () => {
-      let inputs = map(Coin.of, [1, 2, 3])
-      let targets = map(Coin.of, [10000])
-      let selection = cs.findTarget(targets, 0, inputs)
+    it('should return the right selection without feePerByte set', () => {
+      const inputs = map(Coin.fromJS, [
+        { value: 1 },
+        { value: 2 },
+        { value: 3 }
+      ])
+      const targets = map(Coin.fromJS, [{ value: 10000 }])
+      const selection = cs.findTarget(targets, 0, inputs)
       expect(selection.fee).toEqual(0)
       expect(selection.inputs).toEqual([])
       expect(selection.outputs).toEqual(targets)
     })
-    it('should return the right selection', () => {
-      let inputs = map(Coin.of, [1, 20000, 300000])
-      let targets = map(Coin.of, [10000])
-      let selection = cs.findTarget(targets, 55, inputs)
-      expect(selection.fee).toEqual(18590)
+    it('should return the right selection with feePerByte set', () => {
+      const inputs = map(Coin.fromJS, [
+        { value: 1 },
+        { value: 20000 },
+        { value: 300000 }
+      ])
+      const targets = map(Coin.fromJS, [{ value: 10000 }])
+      const feePerByte = 55
+      const selection = cs.findTarget(targets, feePerByte, inputs)
+
+      const estimatedSize = 10 + 2 * 148 + 2 * 34 // 374
+      const estimatedFee = estimatedSize * feePerByte // 205700
+
+      expect(selection.fee).toEqual(estimatedFee)
       expect(selection.inputs.map(x => x.value)).toEqual([20000, 300000])
-      expect(selection.outputs.map(x => x.value)).toEqual([10000, 291410])
+      expect(selection.outputs.map(x => x.value)).toEqual([
+        10000,
+        300000 + 20000 - 10000 - estimatedFee
+      ])
     })
   })
 
   describe('selectAll', () => {
-    it('should return the right selection', () => {
-      let inputs = map(Coin.of, [1, 20000, 0, 0, 300000])
-      let selection = cs.selectAll(55, inputs)
-      expect(selection.fee).toEqual(18590)
+    it('should return the right selection with inputs', () => {
+      const inputs = map(Coin.fromJS, [
+        { value: 1 },
+        { value: 20000 },
+        { value: 0 },
+        { value: 0 },
+        { value: 300000 }
+      ])
+
+      const selection = cs.selectAll(55, inputs)
       expect(selection.inputs.map(x => x.value)).toEqual([20000, 300000])
-      expect(selection.outputs.map(x => x.value)).toEqual([301410])
+
+      // overhead + inputs + outputs
+      // 55 * (10 + 148 * 2 + 34 * 1) = 55 * 340 = 18700
+      expect(selection.fee).toEqual(18700)
+
+      // change = inputs - outputs - fee
+      // 20000 + 300000 - 18700 = 301300
+      expect(selection.outputs.map(x => x.value)).toEqual([301300])
     })
-    it('should return the right selection', () => {
-      let inputs = map(Coin.of, [])
-      let selection = cs.selectAll(55, inputs)
+    it('should return the right selection without inputs', () => {
+      const inputs = map(Coin.fromJS, [])
+      const selection = cs.selectAll(55, inputs)
       expect(selection.fee).toEqual(0)
       expect(selection.inputs.map(x => x.value)).toEqual([])
       expect(selection.outputs.map(x => x.value)).toEqual([0])
@@ -264,23 +301,52 @@ describe('Coin Selection', () => {
 
   describe('descentDraw', () => {
     it('should return the right selection', () => {
-      let inputs = map(Coin.of, [1, 20000, 0, 0, 300000, 50000, 30000])
-      let targets = map(Coin.of, [100000])
-      let selection = cs.descentDraw(targets, 55, inputs, 'change-address')
-      expect(selection.fee).toEqual(10505)
-      expect(selection.inputs.map(x => x.value)).toEqual([300000])
-      expect(selection.outputs.map(x => x.value)).toEqual([100000, 189495])
+      const inputs = map(Coin.fromJS, [
+        { value: 1 },
+        { value: 20000 },
+        { value: 0 },
+        { value: 0 },
+        { value: 300000 },
+        { value: 50000 },
+        { value: 30000 }
+      ])
+      const targets = map(Coin.fromJS, [{ value: 100000 }])
+      const selection = cs.descentDraw(targets, 55, inputs, 'change-address')
+
+      expect(selection.inputs.map(x => x.value)).toEqual([20000, 300000])
+
+      // overhead + inputs + outputs
+      // 55 * (10 + 148 * 2 + 34 * 2) = 20570
+      expect(selection.fee).toEqual(20570)
+
+      // change = inputs - outputs - fee
+      // 20000 + 300000 - 100000 - 20570 = 199430
+      expect(selection.outputs.map(x => x.value)).toEqual([100000, 199430])
     })
   })
 
   describe('ascentDraw', () => {
     it('should return the right selection', () => {
-      let inputs = map(Coin.of, [1, 20000, 0, 0, 300000, 50000, 30000])
-      let targets = map(Coin.of, [100000])
-      let selection = cs.ascentDraw(targets, 55, inputs, 'change-address')
-      expect(selection.fee).toEqual(34760)
-      expect(selection.inputs.map(x => x.value)).toEqual([20000, 30000, 50000, 300000])
-      expect(selection.outputs.map(x => x.value)).toEqual([100000, 265240])
+      const inputs = map(Coin.fromJS, [
+        { value: 1 },
+        { value: 20000 },
+        { value: 0 },
+        { value: 0 },
+        { value: 300000 },
+        { value: 50000 },
+        { value: 30000 }
+      ])
+      const targets = map(Coin.fromJS, [{ value: 100000 }])
+      const selection = cs.ascentDraw(targets, 55, inputs, 'change-address')
+      expect(selection.inputs.map(x => x.value)).toEqual([20000, 300000])
+
+      // overhead + inputs + outputs
+      // 55 * (10 + 148 * 2 + 34 * 2) = 20570
+      expect(selection.fee).toEqual(20570)
+
+      // change = inputs - outputs - fee
+      // 20000 + 300000 - 100000 - 20570 = 199430
+      expect(selection.outputs.map(x => x.value)).toEqual([100000, 199430])
     })
   })
 })
